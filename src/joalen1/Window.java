6743ae5467a277086eddbe99bf8be76740cff46a
joalen1 <56613990+joalen1@users.noreply.github.com>
2019-10-18 15:04:20 -0400

    Make window!

---
+++ src/joalen1/Window.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Window {
+
+	public static void main(String args[]) {
+		JFrame f= new JFrame();
+		f.setVisible(true);
+		f.setSize(100,200); // setting size of the window
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
